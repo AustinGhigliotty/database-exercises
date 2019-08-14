d6c3879eeef4092180e06c491862fe9f93a0cbe4
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-08-14 11:12:05 -0500

    step 1 complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 DROP table if exists albums;
-CREATE table IF NOT EXISTS albums (
-    'id' int unsigned NOT NULL AUTO_INCREMENT,
-    'artist' varchar(50) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'release_date' int NOT NULL,
-    'sales' float NOT NULL,
-    'genre' varchar(50) NOT NULL,
-    PRIMARY KEY ('id')
+CREATE table albums(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    artist varchar(100) NOT NULL,
+    name varchar(150) NOT NULL,
+    release_date int NOT NULL,
+    sales float,
+    genre varchar(50),
+    PRIMARY KEY (id)
 );
